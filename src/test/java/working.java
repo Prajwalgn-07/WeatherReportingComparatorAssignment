c1a76d2f9599ae08c2f0ed1206a416cd118857ca
Prajwalgn-07 <prajwalg@testvagrant.com>
2021-08-14 13:35:33 +0530

    created gitignore and added some folders to it

--- src/test/java/working.java
+++ src/test/java/working.java
@@ -1,27 +1,10 @@
-import Data.UiWeatherData;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import pages.HomePage;
-import pages.WeatherPage;
+import DetailsUsingApi.GetWeatherDetails;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
 
 public class working {
 
-    public static void main(String[] args) {
-        WebDriverManager.chromedriver().setup();
-        Map<String, Object> prefs = new HashMap<String, Object>();
-        prefs.put("profile.default_content_setting_values.notifications", 2);
-        ChromeOptions options = new ChromeOptions();
-        WebDriver driver = new ChromeDriver(options);
-        HomePage homePage=new HomePage(driver);
-        WeatherPage weatherPage=new WeatherPage(driver);
-        driver.get("https://www.accuweather.com/");
-        homePage.searchLocation("bangalore");
-        UiWeatherData uiWeatherData=weatherPage.CurrentWeatherDetails();
-        System.out.println(uiWeatherData);
+    public static void main(String[] args) throws IOException {
+        new GetWeatherDetails().getWeatherDetails("Mangalore");
     }
 }
