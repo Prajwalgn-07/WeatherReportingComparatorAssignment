c0253bcaa15edf831cd45490cad7078079354e43
Prajwalgn-07 <prajwalg@testvagrant.com>
2021-08-16 23:10:20 +0530

    changed the way of accessing headless and blocking the notification

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,54 +4,57 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import utils.PropertyReader;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BaseTest {
     public static WebDriver driver;
     PropertyReader propertyReader;
     public BaseTest() throws IOException {
-        propertyReader=new PropertyReader("/Users/prajwal/Desktop/testvagrant /assignment projects/WeatherReportingComparatorAssignment/src/main/resources/WebPageLinks.properties");
+        propertyReader=new PropertyReader("/Users/prajwal/Desktop/testvagrant /assignment projects/WeatherReportingComparatorAssignment/src/main/resources/WebPage.properties");
     }
     public void initializeBrowser(){
         String browser=System.getProperty("browser");
         String headless=System.getProperty("headless");
         if(browser.equals("chrome")){
             WebDriverManager.chromedriver().setup();
+            Map<String,Object>prefs=new HashMap<>();
+            prefs.put("profile.default_context_setting_values.notifications",propertyReader.getProperty("Notification"));
+            ChromeOptions options=new ChromeOptions();
+            options.setExperimentalOption("prefs",prefs);
             if(headless.equals("True")){
-                ChromeOptions options=headLessChrome();
-                driver=new ChromeDriver(options);
+                options.setHeadless(true);
             }
-            else {
-                driver = new ChromeDriver();
-            }
+            driver=new ChromeDriver(options);
         }
         else if(browser.equals("firefox")){
             WebDriverManager.firefoxdriver().setup();
+            FirefoxProfile profile=new FirefoxProfile();
+            profile.setPreference("permissions.default.desktop-notification", propertyReader.getProperty("Notification"));
+            FirefoxOptions firefoxOptions=new FirefoxOptions();
+            firefoxOptions.setCapability(FirefoxDriver.PROFILE, profile);
             if(headless.equals("True")){
-                FirefoxOptions options=headLessFireFox();
-                driver=new FirefoxDriver(options);
+                firefoxOptions.setCapability("headless",true);
             }
-            else {
-                driver = new FirefoxDriver();
-            }
+            driver=new FirefoxDriver(firefoxOptions);
         }
         else{
             WebDriverManager.chromedriver().setup();
-            driver=new ChromeDriver();
+            Map<String,Object>prefs=new HashMap<>();
+            prefs.put("profile.default_context_setting_values.notifications",propertyReader.getProperty("Notification"));
+            ChromeOptions options=new ChromeOptions();
+            options.setExperimentalOption("prefs",prefs);
+            driver=new ChromeDriver(options);
         }
         driver.manage().window().maximize();
         driver.get(propertyReader.getProperty("accuWeatherPageLink"));
     }
-    public ChromeOptions headLessChrome(){
-        ChromeOptions options=new ChromeOptions();
-        options.setHeadless(true);
-        return options;
-    }
-    public FirefoxOptions headLessFireFox(){
-        FirefoxOptions options=new FirefoxOptions();
-        options.setHeadless(true);
-        return options;
+    public double doublePartInString(String data){
+        return Double.parseDouble(data.replaceAll("[^0-9\\.]", "").trim());
     }
 }
