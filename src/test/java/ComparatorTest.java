34f426f146f058dbfc212343f0c125c6ecc16add
Prajwalgn-07 <prajwalg@testvagrant.com>
2021-08-17 17:55:28 +0530

    data provider and taking screenshot for failed testcases

--- src/test/java/ComparatorTest.java
+++ src/test/java/ComparatorTest.java
@@ -1,34 +1,56 @@
 import Data.ApiWeatherData;
 import Data.UiWeatherData;
 import DetailsUsingApi.GetWeatherDetails;
-import org.testng.Assert;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.WeatherPage;
-
+import java.io.File;
 import java.io.IOException;
 
 public class ComparatorTest extends BaseTest{
+    @DataProvider(name = "cityNames")
+    public Object[][] cities(){
+        return new Object[][]{{"Bengaluru"},{"Mumbai"},{"Kolkata"},{"Delhi"}
+        };
+    }
     public ComparatorTest() throws IOException {
         super();
+    }
+    @BeforeMethod
+    public void setUp(){
         initializeBrowser();
     }
-    @Test
-    public void main() throws IOException {
-        new HomePage(driver).searchLocation("Bangalore");
+    @Test(dataProvider = "cityNames")
+    public void main(String cityNames) throws IOException {
+        new HomePage(driver).searchLocation(cityNames);
         UiWeatherData uiWeatherData=new WeatherPage(driver).CurrentWeatherDetails();
-        ApiWeatherData apiWeatherData=new GetWeatherDetails().getWeatherDetails("Bangalore");
-        System.out.println("The Temperature difference between ui and api is "+ Math.abs((doublePartInString(uiWeatherData.getUiTemperature())
-        -doublePartInString(apiWeatherData.getApiTemperature())))+"Centigrade");
-        System.out.println("The Humidity difference between ui and api is "+Math.abs(doublePartInString(uiWeatherData.getUiHumidity())
+        ApiWeatherData apiWeatherData=new GetWeatherDetails().getWeatherDetails(cityNames);
+        System.out.println("The Temperature difference between ui and api is: %.2f"+ Math.abs((doublePartInString(uiWeatherData.getUiTemperature())
+                -doublePartInString(apiWeatherData.getApiTemperature())))+"Centigrade");
+        System.out.println("The Humidity difference between ui and api is: %.2f"+Math.abs(doublePartInString(uiWeatherData.getUiHumidity())
                 -doublePartInString(apiWeatherData.getApiHumidity()))+"%");
-        System.out.println("The Visibility difference between ui and api is "+Math.abs(doublePartInString(uiWeatherData.getUiVisibility())
-        -doublePartInString(apiWeatherData.getApiVisibility()))+"km");
+        System.out.println("The Visibility difference between ui and api is: %.2f"+Math.abs(doublePartInString(uiWeatherData.getUiVisibility())
+                -doublePartInString(apiWeatherData.getApiVisibility()))+"km");
     }
     @AfterMethod
-    public void tearDown(){
+    public void tearDown(ITestResult result){
+        if(ITestResult.FAILURE==result.getStatus()){
+            TakesScreenshot screenshot=((TakesScreenshot) driver);
+            File sourceFile=screenshot.getScreenshotAs(OutputType.FILE);
+            File destinationFile=new File("/Users/prajwal/Desktop/testvagrant /assignment projects/WeatherReportingComparatorAssignment/src/main/java/utils/FailedTestCases/"+".jpeg");
+            try {
+                FileUtils.copyFile(sourceFile,destinationFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         driver.quit();
     }
 }
