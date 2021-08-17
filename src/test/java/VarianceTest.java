b119d2bcb4c8b1147d9f17d78a983973db081395
Prajwalgn-07 <prajwalg@testvagrant.com>
2021-08-17 17:56:54 +0530

    taking screenshot for failed testcases

--- src/test/java/VarianceTest.java
+++ src/test/java/VarianceTest.java
@@ -1,15 +1,17 @@
 import Data.ApiWeatherData;
 import Data.UiWeatherData;
 import DetailsUsingApi.GetWeatherDetails;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Ignore;
-import org.testng.annotations.Test;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
 import pages.HomePage;
 import pages.WeatherPage;
 import utils.PropertyReader;
 
+import java.io.File;
 import java.io.IOException;
 
 public class VarianceTest extends BaseTest{
@@ -23,12 +25,14 @@ public class VarianceTest extends BaseTest{
     public double temperatureDifference;
     public double humidityDifference;
     public double visibilityDifference;
+
     @BeforeMethod
-    public void getDetails() throws IOException {
+    @Parameters("city")
+    public void getDetails(String city) throws IOException {
         propertyReader=new PropertyReader("/Users/prajwal/Desktop/testvagrant /assignment projects/WeatherReportingComparatorAssignment/src/main/resources/Variance.properties");
-        new HomePage(driver).searchLocation("Bangalore");
+        new HomePage(driver).searchLocation(city);
         uiWeatherData=new WeatherPage(driver).CurrentWeatherDetails();
-        apiWeatherData=new GetWeatherDetails().getWeatherDetails("Bangalore");
+        apiWeatherData=new GetWeatherDetails().getWeatherDetails(city);
         this.temperatureDifference=Math.abs((doublePartInString(uiWeatherData.getUiTemperature())
                 -doublePartInString(apiWeatherData.getApiTemperature())));
         this.humidityDifference=Math.abs(doublePartInString(uiWeatherData.getUiHumidity())
@@ -36,22 +40,32 @@ public class VarianceTest extends BaseTest{
         this.visibilityDifference=Math.abs(doublePartInString(uiWeatherData.getUiVisibility())
                                   -doublePartInString(apiWeatherData.getApiVisibility()));
     }
-    @Ignore
     @Test
     public void temperatureVariance() throws IOException {
         Assert.assertTrue(temperatureDifference < Double.parseDouble(propertyReader.getProperty("temperatureVariance")));
     }
+    @Ignore
     @Test
     public void humidityVariance(){
-        Assert.assertTrue(humidityDifference <= Double.parseDouble(propertyReader.getProperty("humidityVariance")));
+        Assert.assertTrue(humidityDifference < Double.parseDouble(propertyReader.getProperty("humidityVariance")));
     }
     @Ignore
     @Test
     public void visibilityVariance(){
-        Assert.assertTrue(visibilityDifference > Double.parseDouble(propertyReader.getProperty("visibilityVariance")));
+        Assert.assertTrue(visibilityDifference < Double.parseDouble(propertyReader.getProperty("visibilityVariance")));
     }
     @AfterMethod
-    public void tearDown(){
+    public void tearDown(ITestResult result){
+        if(ITestResult.FAILURE==result.getStatus()){
+            TakesScreenshot screenshot=((TakesScreenshot) driver);
+            File sourceFile=screenshot.getScreenshotAs(OutputType.FILE);
+            File destinationFile=new File("/Users/prajwal/Desktop/testvagrant /assignment projects/WeatherReportingComparatorAssignment/src/main/java/utils/FailedTestCases/"+".jpeg");
+            try {
+                FileUtils.copyFile(sourceFile,destinationFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         driver.quit();
     }
 }
